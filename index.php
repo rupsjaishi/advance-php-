c9851f6ae056e392293a2e71d83049f942b5c0bd
rupesh <jaishirupesh@gmail.com>
2018-03-05 17:18:25 +0545

    Added bootstrap framwork.

--- index.php
+++ index.php
@@ -2,8 +2,17 @@
 <html>
 <head>
 	<title>My First Project</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 </head>
 <body>
 	<h1>Welcome </h1>
+	<h3>Courses</h3>
+	<ul>
+		<li>Basic PHP</li>
+		<li>Advance PHP</li>
+
+		<li>Laravel</li>
+
+	</ul>
 </body>
 </html>
